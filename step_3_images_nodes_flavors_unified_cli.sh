223f32b2609ff58076c8036ffb803629b215300c
Dougal Matthews <dougal@redhat.com>
2015-05-26 12:56:36 +0100

    Add baremetal import and introspection

--- step_3_images_nodes_flavors_unified_cli.sh
+++ step_3_images_nodes_flavors_unified_cli.sh
@@ -14,8 +14,12 @@ export RUN_RHOS_RELEASE=1
 openstack overcloud image build --all # TODO: does not work now
 openstack overcloud image create
 
-instack-ironic-deployment --nodes-json instackenv.json --register-nodes
-instack-ironic-deployment --discover-nodes
+openstack baremetal import --json ~/instackenv.json
+openstack baremetal introspection bulk start # TODO: doesn't wait for introspection to finish
+
+# Use this until the above is fixed, then they are all finished.
+watch openstack baremetal introspection bulk status
+
 instack-ironic-deployment --setup-flavors
 
 echo "  Next step is overcloud deploy..."
