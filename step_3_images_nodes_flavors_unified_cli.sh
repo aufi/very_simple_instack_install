7d668d0533062caa7b450e5aa19ab7fb573ddc3d
Dougal Matthews <dougal@redhat.com>
2015-05-27 09:25:10 +0100

    Added notes about workarounds

--- step_3_images_nodes_flavors_unified_cli.sh
+++ step_3_images_nodes_flavors_unified_cli.sh
@@ -11,14 +11,14 @@ curl -O http://download.devel.redhat.com/brewroot/packages/rhel-guest-image/7.1/
 export DIB_LOCAL_IMAGE=rhel-guest-image-7.1-20150203.1.x86_64.qcow2
 export RUN_RHOS_RELEASE=1
 
-openstack overcloud image build --all # TODO: does not work now - Should be fixed
-openstack overcloud image upload # updated, previous was create
+openstack overcloud image build --all # If this fails and you are on RHEL this should work: openstack overcloud image build --all --run-rhos-release
+openstack overcloud image upload # If this fails use the old name: openstack overcloud image create
 
 openstack baremetal import --json ~/instackenv.json
-openstack baremetal introspection bulk start # TODO: doesn't wait for introspection to finish - fix merged
+openstack baremetal introspection bulk start
 
-# Use this until the above is fixed, then they are all finished. - Fix merged
-# watch openstack baremetal introspection bulk status
+# If the above command doesn't wait for introspection to finish, use this:
+watch openstack baremetal introspection bulk status
 
 openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 1 baremetal
 openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" baremetal
