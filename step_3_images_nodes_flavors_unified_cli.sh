732b3a5d030404fb9013e0cb804b5e296218b325
Marek Aufart <aufi.cz@gmail.com>
2015-05-27 10:08:21 +0200

    renaming image overcloud create to update

--- step_3_images_nodes_flavors_unified_cli.sh
+++ step_3_images_nodes_flavors_unified_cli.sh
@@ -12,7 +12,7 @@ export DIB_LOCAL_IMAGE=rhel-guest-image-7.1-20150203.1.x86_64.qcow2
 export RUN_RHOS_RELEASE=1
 
 openstack overcloud image build --all # TODO: does not work now - Should be fixed
-openstack overcloud image create
+openstack overcloud image upload # updated, previous was create
 
 openstack baremetal import --json ~/instackenv.json
 openstack baremetal introspection bulk start # TODO: doesn't wait for introspection to finish - fix merged
