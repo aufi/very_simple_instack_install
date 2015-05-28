7eb066007e000393d9e059fc5246698503921b6c
Marek Aufart <aufi.cz@gmail.com>
2015-05-28 11:09:43 +0200

    Update step_1_virt_env_setup.sh

--- step_1_virt_env_setup.sh
+++ step_1_virt_env_setup.sh
@@ -6,8 +6,13 @@ sudo chmod 0440 /etc/sudoers.d/stack
 
 su - stack
 
-sudo yum install -y http://rhos-release.virt.bos.redhat.com/repos/rhos-release/rhos-release-latest.noarch.rpm
-sudo rhos-release 6
+sudo rpm -ivh http://rhos-release.virt.bos.redhat.com/repos/rhos-release/rhos-release-latest.noarch.rpm
+
+# Enable either poodles or puddles:
+# To enable poodles: CHOOSE
+sudo rhos-release 7-director -d
+# To enable puddles:
+#sudo rhos-release 7-director
 
 # We need openwsman-python from the optional repo
 sudo yum install -y yum-utils
@@ -21,11 +26,12 @@ sudo yum install -y https://rdoproject.org/repos/openstack-kilo/rdo-release-kilo
 
 # Enable RDO-Manager Trunk
 sudo curl -o /etc/yum.repos.d/rdo-management-trunk.repo http://trunk-mgt.rdoproject.org/centos-kilo/current-passed-ci/delorean-rdo-management.repo
+
 sudo yum install -y instack-undercloud
 
 curl -O http://download.devel.redhat.com/brewroot/packages/rhel-guest-image/7.1/20150203.1/images/rhel-guest-image-7.1-20150203.1.x86_64.qcow2
 export DIB_LOCAL_IMAGE=rhel-guest-image-7.1-20150203.1.x86_64.qcow2
-export DIB_YUM_REPO_CONF=/etc/yum.repos.d/rhos-release-6-rhel-7.1.repo
+export DIB_YUM_REPO_CONF="/etc/yum.repos.d/rhos-release-7-director-rhel-7.1.repo /etc/yum.repos.d/rhos-release-7-rhel-7.1.repo"
 
 instack-virt-setup
 
