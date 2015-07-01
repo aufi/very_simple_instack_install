5fc02947d09aa6a2f1663b75414321bd9dd92dbb
Lennart Regebro <regebro@gmail.com>
2015-07-01 10:39:04 +0200

    Switching to a RHEL image that exists

--- step_3_images_nodes_flavors_unified_cli.sh
+++ step_3_images_nodes_flavors_unified_cli.sh
@@ -6,8 +6,8 @@ source stackrc
 
 export NODE_DIST=rhel7
 
-curl -O http://download.devel.redhat.com/brewroot/packages/rhel-guest-image/7.1/20150203.1/images/rhel-guest-image-7.1-20150203.1.x86_64.qcow2
-export DIB_LOCAL_IMAGE=rhel-guest-image-7.1-20150203.1.x86_64.qcow2
+curl -O http://download.devel.redhat.com/brewroot/packages/rhel-guest-image/7.1/20150224.0/images/rhel-guest-image-7.1-20150224.0.x86_64.qcow2
+export DIB_LOCAL_IMAGE=rhel-guest-image-7.1-20150224.0.x86_64.qcow2
 # Enable RHOS
 export USE_DELOREAN_TRUNK=0
 export RHOS=1
