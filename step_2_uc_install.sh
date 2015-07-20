dfc677c90744b8179b89bd4f37d7e1ed9b1fba8c
Marek Aufart <aufi.cz@gmail.com>
2015-07-20 14:22:37 +0200

    update step 2 to puddle

--- step_2_uc_install.sh
+++ step_2_uc_install.sh
@@ -1,26 +1,15 @@
-#http://repos.fedorapeople.org/repos/openstack-m/instack-undercloud/internal-html/install-undercloud.html
+# https://repos.fedorapeople.org/repos/openstack-m/docs/internal/master/installation/installing.html
 su - stack
 
 sudo rpm -ivh http://rhos-release.virt.bos.redhat.com/repos/rhos-release/rhos-release-latest.noarch.rpm
 
 # Enable either poodles or puddles:
 # To enable poodles: CHOOSE
-sudo rhos-release 7-director -d
+# sudo rhos-release 7-director -d
 # To enable puddles:
-# sudo rhos-release 7-director
+sudo rhos-release 7-director
 
-
-# We need openwsman-python from the optional repo
-sudo yum install -y yum-utils
-sudo yum-config-manager --enable rhelosp-rhel-7-server-opt
-
-# Enable RDO Kilo
-sudo yum install -y https://rdoproject.org/repos/openstack-kilo/rdo-release-kilo.rpm
-
-# Enable RDO-Manager Trunk
-sudo curl -o /etc/yum.repos.d/rdo-management-trunk.repo http://trunk-mgt.rdoproject.org/centos-kilo/current-passed-ci/delorean-rdo-management.repo
-
+# Install the RDO Manager CLI
 sudo yum install -y python-rdomanager-oscplugin
 
 openstack undercloud install
-
